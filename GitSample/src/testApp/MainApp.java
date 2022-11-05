3389cca15907e25839833e0a8fbd1817ec632ab8
Billy <lovepin0318@gmail.com>
2022-11-05 14:52:08 +0800

    Add Math Branch

--- GitSample/src/testApp/MainApp.java
+++ GitSample/src/testApp/MainApp.java
@@ -4,6 +4,16 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("2nd Version");
+		
+		//組長新增的 Code
+		Math cMath = new Math();
+		System.out.println();
+		System.out.print(cMath.add(5, 5));
 	}
 
+}
+class Math {
+	int add(int x, int y) {
+		return x + y;
+	}
 }
